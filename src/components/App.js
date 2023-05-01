21e0692e8a84ae644f5f920a04c376ba3427b0df
Nick-Guzy <nick.guzy76@gmail.com>
2023-05-01 16:24:39 -0700

    ENV works

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from "./Header";
 import TicketControl from "./TicketControl";
 
 function App(){
+  console.log(process.env.REACT_APP_FIREBASE_API_KEY)
   return (
     <React.Fragment>
       <Header />
